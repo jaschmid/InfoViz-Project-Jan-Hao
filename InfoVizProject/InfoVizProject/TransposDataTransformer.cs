3335db4463d9422d38096a297c72e6ff2a069019
haozh599 <zhonghaoemail@gmail.com>
2011-03-23 22:54:01 +0100

    sorry for miss this file

---
+++ InfoVizProject/InfoVizProject/TransposDataTransformer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gav.Data;
+using Gav.Transformers;
+
+namespace InfoVizProject
+{
+    class TransposDataTransformer: DataTransformer
+    {
+        private int selectedIndicator;
+        public int SelectedIndicator
+        {
+            set { selectedIndicator = value; }
+            get { return selectedIndicator; }
+        }
+        private List<int> selectedCountry;
+        public List<int> SelectedCountry
+        {
+            set { selectedCountry = value;}
+            get {return selectedCountry;}
+        }
+        
+        protected override void ProcessData()
+        {
+            
+            float[, ,] inputData = _input.GetDataCube().DataArray;
+            int sizeX = inputData.GetLength(0);
+            int sizeY = inputData.GetLength(1);
+            int sizeZ = inputData.GetLength(2);
+            int countryNum = selectedCountry.Count;
+            float[, ,] outputData = new float[countryNum, sizeZ, 1];
+           
+                for(int j = 0;j<countryNum;j++)
+                {
+                    for (int k = 0; k < sizeZ; k++)
+                    {
+                        outputData[j, k, 0] = inputData[selectedIndicator,selectedCountry[j] ,k];
+                    }
+                }
+            
+            
+            
+            _dataCube.DataArray = outputData;
+        }
+    }
+}
